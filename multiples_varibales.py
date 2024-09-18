6afc7b0665a0bc21572d7237f70383cd5fa770c5
Alejo2004 <73247529+Alejo2004@users.noreply.github.com>
2024-09-18 15:43:44 -0500

    5_Multiples_Variables

---
+++ multiples_varibales.py
@@ -0,0 +1,21 @@
+#Many Values to Multiple Variables
+a, b, c, d = "Carro", "Moto", "Avion", "Barco"  #A cada variable se le va asignado un valor respectivamente
+print(a)
+print(b)
+print(c)
+print(d)
+
+#One Value to Multiple Variables
+e = f = g = h = "Helicoptero"      #A cada variable se le asigna un unico valor
+print(e)
+print(f)
+print(g)
+print(h)
+
+#Unpack a Collection
+figuras = ["Triangulo", "Cuadrado", "Circulo", "Rombo"]          
+i, j, k, l = figuras        #Para cada variable se almacena por orden cada elemento que hay en la lista de figuras
+print(i)
+print(j)
+print(k)
+print(l) 
