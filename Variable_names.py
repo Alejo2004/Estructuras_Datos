fd88e96cf581f7d75849248d7ffa4b35dfa8e2d8
Alejo2004 <73247529+Alejo2004@users.noreply.github.com>
2024-09-18 15:35:11 -0500

    4_Variables_Names

---
+++ Variable_names.py
@@ -0,0 +1,17 @@
+variable1 = "Manuel"
+print(variable1)        #Si vamos a guardar variables con espacios entre ellos se coloca "_"
+variable_1 = "Error"
+print(variable_1)
+
+#Hay 3 tipos en los cuales podemos nombrar variables
+#Camel Case
+primeravariable = "Alejandro"
+print(primeravariable)      #No importa 
+
+#Pascal Case
+Primeravariable = "Logan"
+print(Primeravariable)
+
+#Snake Case
+primera_variable = "Lucas"
+print(primera_variable)
