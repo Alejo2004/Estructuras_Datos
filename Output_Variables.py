c693f585f71e70c660b678abb348638d61eab638
Alejo2004 <73247529+Alejo2004@users.noreply.github.com>
2024-09-18 15:55:29 -0500

    6_Output_variables

---
+++ Output_Variables.py
@@ -0,0 +1,19 @@
+#Output Variables
+s = "Eres Increible"
+print(s)                #Imprime el valor de s
+
+a = "Eres"
+b = "Increible"
+print(a, b)         #Para imprimir variables variables se separan por ","
+
+a = "Eres"
+b = "Increible"
+print(a + b)        #Para concatenar variables utilizamos el simbolo "+"
+
+r = 90
+m = 100
+print(r + m)        #En este caso el simbolo "+" lo que hace es una operacion matematica
+
+h = "Lucas"
+k = 700
+print(h,k)          #En este caso para imprimir un valor entero con un caracter utilizamos "," ya que si se utiliza el simbolo "+" nos muestra un error
