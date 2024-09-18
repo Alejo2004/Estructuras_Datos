d271126f7b940dd51b9fc727dc5cd32ac8840350
Alejo2004 <73247529+Alejo2004@users.noreply.github.com>
2024-09-18 15:28:12 -0500

    3_Variables phyton

---
+++ Variables.py
@@ -0,0 +1,31 @@
+#Variables Phyton
+x = 50
+y = "Alejo"
+print(x)
+print(y)
+
+#Casting
+z = int(8)      #Define como entero a la variable
+a = str(8)      #Define como caracter a la variable
+b = float(8)    #Define como flotante a la variable
+print(z)
+print(a)
+print(b)
+
+#Get the type
+c = 89
+d = "Jose"
+#Muestra el tipo de variable
+print(type(c))
+print(type(d))
+
+#Single or Double Quotes
+i = "Alejandro"
+i = 'Alejandro'
+print(i)        #No importa si utiliza doble comillas o comillas simples seria igual
+
+#Case-Sensitive
+s = 9
+S = "Paul"
+print(s)
+print(S)        #No transcribe S a s por que son diferentes
