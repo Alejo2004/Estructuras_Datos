1162472d74bbe2383d7d9597c81158cc8bc1902f
Alejo2004 <73247529+Alejo2004@users.noreply.github.com>
2024-09-19 15:30:17 -0500

    8_DataTypes

---
+++ Data_Type.py
@@ -0,0 +1,154 @@
+#Getting the Data Type
+x = 200
+print(type(x))
+
+#Setting the Data Type
+#String
+x = "Hello World"
+print(x)
+print(type(x))
+
+#Int
+a = 30
+print(a)
+print(type(a))
+
+#Float
+b = 805.69
+print(b)
+print(type(b))
+
+#Complex
+c = 1j
+print(c)
+print(type(c))
+
+#List
+d = ["Rombo", "Triangulo", "Circulo", "Cuadrado"]
+print(d)
+print(type(d))
+
+#Tuple
+e = ("Rombo", "Triangulo", "Circulo", "Cuadrado")
+print(e)
+print(type(e))
+
+#Range
+f = range(8)
+print(f)
+print(type(f))
+
+#Dict
+g = {"Name" : "Alejo", "age" : 36}
+print(g)
+print(type(g))
+
+#Set
+h = {"Manzana", "Banana", "Cereza"}
+print(h)
+print(type(h))
+
+#Frozenset
+i = frozenset({"Apple", "Banana", "Cherry"})
+print(i)
+print(type(i))
+
+#Bool
+j = True
+print(j)
+print(type(j))
+
+#Bytes
+k = b"Hello"
+print(k)
+print(type(k))
+
+#Bytearray
+l = bytearray(6)
+print(l)
+print(type(l))
+
+#Memoryview
+m = memoryview(bytes(6))
+print(m)
+print(type(m))
+
+#NoneType
+n = None
+print(n)
+print(type(n))
+
+#String
+x = str("Hello World")
+print(x)
+print(type(x))
+
+#Int
+a = int(30)
+print(a)
+print(type(a))
+
+#Float
+b = float(805.69)
+print(b)
+print(type(b))
+
+#Complex
+c = complex(1j)
+print(c)
+print(type(c))
+
+#List
+d = list(("Rombo", "Triangulo", "Circulo", "Cuadrado"))
+print(d)
+print(type(d))
+
+#Tuple
+e = tuple(("Rombo", "Triangulo", "Circulo", "Cuadrado"))
+print(e)
+print(type(e))
+
+#Range
+f = range(8)
+print(f)
+print(type(f))
+
+#Dict
+g = {"Name" : "Alejo", "age" : 36}
+print(g)
+print(type(g))
+
+#Set
+h = set(("Manzana", "Banana", "Cereza"))
+print(h)
+print(type(h))
+
+#Frozenset
+i = frozenset(("Apple", "Banana", "Cherry"))
+print(i)
+print(type(i))
+
+#Bool
+j = bool(5)
+print(j)
+print(type(j))
+
+#Bytes
+k = bytes(5)
+print(k)
+print(type(k))
+
+#Bytearray
+l = bytearray(6)
+print(l)
+print(type(l))
+
+#Memoryview
+m = memoryview(bytes(6))
+print(m)
+print(type(m))
+
+#NoneType
+n = None
+print(n)
+print(type(n))
